1f7351caf49ddf96ba29fb0a75e014ee991a94fe
toygun06 <toyguny@hotmail.com>
2024-04-20 20:28:29 +0300

    Seed Data & OnModelCreating

--- DataAccess/Abstracts/ICategoryRepository.cs
+++ DataAccess/Abstracts/ICategoryRepository.cs
@@ -1,4 +1,4 @@
-﻿using Entities;
+﻿ using Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- DataAccess/Concretes/EntityFramework/BaseDbContext.cs
+++ DataAccess/Concretes/EntityFramework/BaseDbContext.cs
@@ -14,5 +14,26 @@ namespace DataAccess.Concretes.EntityFramework
                 "Trusted_Connection=True");
             base.OnConfiguring(optionsBuilder);
         }
+
+        //Spesifik konfigürasyonlar vermek için OnModelCreating kullanılır.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().ToTable("Products");
+            modelBuilder.Entity<Product>().HasOne(i => i.Category);
+            modelBuilder.Entity<Product>().Property(i => i.Name).HasColumnName("Name").HasMaxLength(50);
+
+            //Seed Data
+            //Veritabanını oluşturduğumuzda kontrol etmek için deneme verileri
+            int categoryId = 0;
+            Category category = new Category(++categoryId, "Giyim");
+            Category category1 = new(++categoryId, "Elektronik");
+
+            Product product = new Product(1, "Kazak", 500, 50, 1);
+
+            modelBuilder.Entity<Category>().HasData(category, category1);
+            modelBuilder.Entity<Product>().HasData(product);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
