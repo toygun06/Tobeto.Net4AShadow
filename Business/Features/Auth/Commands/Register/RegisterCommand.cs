90cbc49933be41b043ac83f1ad663212a1222305
toygun06 <toyguny@hotmail.com>
2024-05-02 02:03:41 +0300

    Add Users

---
+++ Business/Features/Auth/Commands/Register/RegisterCommand.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Features.Auth.Commands.Register
+{
+    public class RegisterCommand : IRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public class RegisterComandHandler : IRequestHandler<RegisterCommand>
+        {
+            private readonly IMapper _mapper;
+
+            public RegisterComandHandler(IMapper mapper)
+            {
+                _mapper = mapper;
+            }
+
+            public Task Handle(RegisterCommand request, CancellationToken cancellationToken)
+            {
+                User user = _mapper.Map<User>(request);
+                user.PasswordHash = null;
+                user.PasswordSalt = null;
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
--- DataAccess/Concretes/EntityFramework/BaseDbContext.cs
+++ DataAccess/Concretes/EntityFramework/BaseDbContext.cs
@@ -8,6 +8,7 @@ namespace DataAccess.Concretes.EntityFramework
     {
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDb; Database=Tobeto4AShadow;" +
